952898db5bef85ecc2d8bc1e4204305c05269926
komugi <noborikawa.ko1996@gmail.com>
2025-11-06 16:09:48 +0900

    Initial commit

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,12 @@
-// @ts-check
+// astro.config.mjs
 import { defineConfig } from 'astro/config';
+import tailwind from "@astrojs/tailwind";
+import icon from "astro-icon"; // ← import があるか確認
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  integrations: [
+    tailwind(),
+    icon() // ← ここに icon() が追加されているか確認
+  ]
+});
